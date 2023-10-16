02408748d5b111907807c5d249d1ce36f420e078
Zhi Gu <zhi.gu.connect@gmail.com>
2023-10-16 16:08:14 +1100

    fix error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:SpiltBill/main.dart';
+import 'package:SplitBill/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
